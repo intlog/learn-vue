66577929034ff9200653945f5f7bfa300c61fc1f
pythonlog <1527563099@qq.com>
2019-01-31 23:38:06 +0800

    基础-异步组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <keep-alive><BaseInput></BaseInput></keep-alive>
     <TodoList :todos="todos">
       <template slot-scope="slotProps">
         <span v-if="slotProps.todo.isComplete">good</span>
